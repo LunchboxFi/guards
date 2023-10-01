4e3cf715bb718bb8283a051caae1a467c9d1410a
Ozodimgba <idasiadiachi@gmail.com>
2023-10-01 07:03:26 +0000

    encrypt and decrypt

--- test.ts
+++ test.ts
@@ -18,24 +18,48 @@ function encrypt(PIN: number, privateKey: any) {
 
   const password = decryptDerive(privateKeyPassword, protocolNonce, value)
 
-  const decryptedArray = decryptionKey(userSecret, protocolNonce, value)
-  // console.log(decryptedArray)
   
+  return {
+    userNonce,
+    protocolNonce,
+    PIN,
+    value
+  }
+  
+}
+
+
+function decrypt(PIN: any, userNonce: any, protocolNonce: any, value: any) {
+
+  const userSecret = scramblePin(PIN, userNonce)
+  const privateKeyPassword = deterministicScrambler(userSecret, protocolNonce)
+  const password = decryptDerive(privateKeyPassword, protocolNonce, value)
+  
+  const decryptedbs58 = decryptionKey(userSecret, protocolNonce, value)
+  const processedPrivateKey = bs58.decode(decryptedbs58)
+
+  return processedPrivateKey
 }
 
 
 
 const key = Keypair.generate().secretKey
-console.log(key)
+
 const address = bs58.encode(key)
-console.log(address)
+// console.log(address)
 // Decode the base58-encoded string
 const decodedBytes = bs58.decode(address);
 
 // Create a Uint8Array from the decoded bytes
-const uint8Array = new Uint8Array(decodedBytes);
+// const uint8Array = new Uint8Array(decodedBytes);
 
-console.log('Uint8Array:', uint8Array);
+// console.log('Uint8Array:', uint8Array);
 const PIN = 9923
 
-encrypt(PIN, key)
+const userNonce = '/Nnu9l=37twAJn=7';
+const protocolNonce = 'n7ungl=L7iMj=93G';
+const value = '973fffdea19c79a1f631c545847a6be737e359895904c33290d5446b65aae97ba56c508b857d5d436a7012d35a1fba40e4b0dd50f4623f83541ad2d0026ff7ca7c5244494839064fd3bc9e793ade4586ec5f5606b1b7d5786f594065bfd418fa'
+
+// decrypt flow
+ const decryptt = decrypt(PIN, userNonce, protocolNonce, value);
+ console.log(decryptt)
