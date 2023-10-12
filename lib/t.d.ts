9c59fd7668b915444716407a922a39704f694ef3
Ife-blink <idnba.asiadiachi@gmail.com>
2023-10-12 14:50:18 +0000

    push spending limits

---
+++ lib/t.d.ts
@@ -0,0 +1,7 @@
+import { Keypair } from '@solana/web3.js';
+import * as web3 from "@solana/web3.js";
+export declare function createMultisig(RPC: web3.Cluster): Promise<{
+    multisig: string;
+    keypairs: Keypair[];
+    signature: any;
+} | undefined>;
