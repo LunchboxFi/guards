5dde7c2ac36ae51b159242ac403028e8d97dc121
Ife-blink <idnba.asiadiachi@gmail.com>
2023-10-12 15:07:41 +0000

    fixed export spending limit

--- lib/cjs/spendingLimits.d.ts
+++ lib/cjs/spendingLimits.d.ts
@@ -1,1 +1,2 @@
-export {};
+import { Cluster, PublicKey } from "@solana/web3.js";
+export default function addSpendingLimit(multisigPda: PublicKey, Cluster: Cluster, token: PublicKey): Promise<void>;
