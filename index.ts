5be79fd958145c10a8dd17cd2b2b5723047f92de
Ozodimgba <75076112+Ozodimgba@users.noreply.github.com>
2023-10-15 20:59:05 +0000

    fix

--- index.ts
+++ index.ts
@@ -3,5 +3,4 @@ import { decrypt } from './cards/decrypt.js'
 import { transferSOL } from './transfer.js'
 import { createMultisig } from './createMultisig.js'
 import { fetchMultisigAccount } from './fetch.js'
-import { addSpendingLimit } from './spendingLimits.js'
-export { encrypt, decrypt, transferSOL, createMultisig, fetchMultisigAccount, addSpendingLimit }
+export { encrypt, decrypt, transferSOL, createMultisig, fetchMultisigAccount }
--- lib/cjs/t.d.ts
+++
@@ -1,7 +0,0 @@
-import { Keypair } from '@solana/web3.js';
-import * as web3 from "@solana/web3.js";
-export declare function createMultisig(RPC: web3.Cluster): Promise<{
-    multisig: string;
-    keypairs: Keypair[];
-    signature: string;
-} | undefined>;
